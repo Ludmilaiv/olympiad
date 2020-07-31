bae5e52b049d390757b0ba1109bde20912d3f520
Ludmila Zhylianova <ludiky30@gmail.com>
2020-07-31 18:38:56 +0700

    Новые уровни, новые блоки

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php
-  session_start();
-  if (isset($_GET['play'])) {
-    require "modules/quest-page.html";
-  } else {
-    require "modules/lending-page.html";
-  }
+<?php
+  session_start();
+  
+  if (isset($_GET['play'])) {
+    require "modules/quest-page3.html";
+  } else {
+    require "modules/lending-page.html";
+  }
 ?>
