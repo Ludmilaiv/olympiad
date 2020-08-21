8edb3dda4a4def8197cc0ea2a3d304275671c57e
Ludmila Zhylianova <ludiky30@gmail.com>
2020-08-22 02:32:52 +0700

    Регистрация и отправка email

---
+++ modules/mail.php
@@ -0,0 +1,14 @@
+<?php
+$to      = $user->email;
+$subject = 'Олимпиада по программированию';
+$message = '<h2>Здравствуй, '.$user->name.'</h2>';
+$message .= '<p>Для участия в олимпиаде перейди по этой ссылке:</p>';
+$message .= '<a href="http://quest.itgen.io/?u='.$user->address.'">quest.itgen.io/?u='.$user->address.'</a>';
+$headers = "From:Itgenio<itgenby@gmail.com>\r\n";
+$headers .= "Content-type: text/html; charset=utf-8 \r\n";
+$mail = (mail($to, $subject, $message, $headers));
+if ($mail!=null) {
+    header ('Refresh:0; url="\?reg='.$user->id);
+}
+else echo "Что-то пошло не так";
+?>
