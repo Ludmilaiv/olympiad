a62ce9dc4a52d996d081d7d61f009c9db1b07ddb
Ludmila Zhylianova <ludiky30@gmail.com>
2020-08-23 17:48:42 +0700

    Отправка почты через SMTP

--- modules/mail.php
+++ modules/mail.php
@@ -1,14 +1,40 @@
-<?php
-$to      = $user->email;
-$subject = 'Олимпиада по программированию';
-$message = '<h2>Здравствуй, '.$user->name.'</h2>';
-$message .= '<p>Для участия в олимпиаде перейди по этой ссылке:</p>';
-$message .= '<a href="http://quest.itgen.io/?u='.$user->address.'">quest.itgen.io/?u='.$user->address.'</a>';
-$headers = "From:Itgenio<itgenby@gmail.com>\r\n";
-$headers .= "Content-type: text/html; charset=utf-8 \r\n";
-$mail = (mail($to, $subject, $message, $headers));
-if ($mail!=null) {
+<?php 
+
+require 'PHPMailer/PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+$mail->isSMTP();
+
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'itgenby@gmail.com';
+$mail->Password = '1Itgeneration1';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
+
+$mail->CharSet = 'UTF-8';
+
+$mail->From = 'itgenby@gmail.com';
+$mail->FromName = 'Itgenio';
+$mail->addAddress($user->email);
+
+$mail->IsHTML(true);
+
+$mail->Subject = 'Олимпиада по программированию';
+$mail->Body = '<h2>Здравствуй, '.$user->name.'</h2>';
+$mail->Body .= '<p>Для участия в олимпиаде перейди по этой ссылке:</p>';
+$mail->Body .= '<a href="http://quest.itgen.io/?u='.$user->address.'">quest.itgen.io/?u='.$user->address.'</a>';
+
+$mail->AltBody = 'Здравствуй, '.$user->name;
+$mail->AltBody .= 'Для участия в олимпиаде перейди по этой ссылке:';
+$mail->AltBody .= $user->address;
+
+if($mail->send()) {
     header ('Refresh:0; url="\?reg='.$user->id);
+}else{
+    echo 'Что-то пошло не так. Письмо не может быть отправлено';
+    echo $mail->ErrorInfo;
 }
-else echo "Что-то пошло не так";
+
 ?>
