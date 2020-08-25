be820fa0cb51c367fd68e151b8402f289994f077
Ludmila Zhylianova <ludiky30@gmail.com>
2020-08-25 11:26:28 +0700

    уровень 7 готов

--- itgenio-blocks/blocks.js
+++ itgenio-blocks/blocks.js
@@ -1,349 +1,349 @@
-//Тут создаются наши пользовательские блоки со всеми их параметрами
-
-//Для создания можно пользоваться этим приложением https://blockly-demo.appspot.com/static/demos/blockfactory/index.html
-
-
-
-Blockly.Blocks['go_right'] = {
-
-  init: function() {
-
-    this.appendDummyInput()
-
-        .appendField("Шаг вправо");
-
-    this.setPreviousStatement(true, null);
-
-    this.setNextStatement(true, null);
-
-    this.setColour(120);
-
- this.setTooltip("Движение");
-
- this.setHelpUrl("");
-
-  }
-
-};
-
-
-
-Blockly.Blocks['go_left'] = {
-
-  init: function() {
-
-    this.appendDummyInput()
-
-        .appendField("Шаг влево");
-
-    this.setPreviousStatement(true, null);
-
-    this.setNextStatement(true, null);
-
-    this.setColour(120);
-
- this.setTooltip("Движение");
-
- this.setHelpUrl("");
-
-  }
-
-};
-
-
-
-Blockly.Blocks['go_up'] = {
-
-  init: function() {
-
-    this.appendDummyInput()
-
-        .appendField("Шаг вверх");
-
-    this.setPreviousStatement(true, null);
-
-    this.setNextStatement(true, null);
-
-    this.setColour(120);
-
- this.setTooltip("Движение");
-
- this.setHelpUrl("");
-
-  }
-
-};
-
-
-
-Blockly.Blocks['go_down'] = {
-
-  init: function() {
-
-    this.appendDummyInput()
-
-        .appendField("Шаг вниз");
-
-    this.setPreviousStatement(true, null);
-
-    this.setNextStatement(true, null);
-
-    this.setColour(120);
-
- this.setTooltip("Движение");
-
- this.setHelpUrl("");
-
-  }
-
-};
-
-
-
-//цикл "Пока ... свободно"
-
-Blockly.Blocks['while_direction_free'] = {
-
-  init: function() {
-
-    this.appendStatementInput("loop_body")
-
-        .setCheck(null)
-
-        .setAlign(Blockly.ALIGN_CENTRE)
-
-        .appendField("Пока  ")
-
-        .appendField(new Blockly.FieldDropdown([["справа","right"], ["слева","left"], ["сверху","top"], ["снизу","down"]]), "direction")
-
-        .appendField("свободно");
-
-    this.setPreviousStatement(true, null);
-
-    this.setNextStatement(true, null);
-
-    this.setColour(150);
-
- this.setTooltip("");
-
- this.setHelpUrl("");
-
-  }
-
-};
-
-
-
-//цикл "Пока цель не достигнута"
-
-Blockly.Blocks['while_not_goal'] = {
-
-  init: function() {
-
-    this.appendStatementInput("loop_body")
-
-        .setCheck(null)
-
-        .setAlign(Blockly.ALIGN_CENTRE)
-
-        .appendField("Пока цель не достигнута")
-
-    this.setPreviousStatement(true, null);
-
-    this.setNextStatement(true, null);
-
-    this.setColour(150);
-
- this.setTooltip("");
-
- this.setHelpUrl("");
-
-  }
-
-};
-
-
-
-//условие "Если ... свободно"
-
-Blockly.Blocks['if_direction_free'] = {
-
-  init: function() {
-
-    this.appendStatementInput("condition_body")
-
-        .setCheck(null)
-
-        .appendField("Если ")
-
-        .appendField(new Blockly.FieldDropdown([["справа","right"], ["слева","left"], ["сверху","top"], ["снизу","down"]]), "direction")
-
-        .appendField("свободно");
-
-    this.setPreviousStatement(true, null);
-
-    this.setNextStatement(true, null);
-
-    this.setColour(180);
-
- this.setTooltip("");
-
- this.setHelpUrl("");
-
-  }
-
-};
-
-
-
-//условие "Если ... свободно, иначе"
-
-Blockly.Blocks['if_direction_free_else'] = {
-
-  init: function() {
-
-    this.appendStatementInput("condition_body")
-
-        .setCheck(null)
-
-        .appendField("Если ")
-
-        .appendField(new Blockly.FieldDropdown([["справа","right"], ["слева","left"], ["сверху","top"], ["снизу","down"]]), "direction")
-
-        .appendField("свободно");
-
-    this.appendStatementInput("else_body")
-
-        .setCheck(null)
-
-        .setAlign(Blockly.ALIGN_RIGHT)
-
-        .appendField("иначе");
-
-    this.setPreviousStatement(true, null);
-
-    this.setNextStatement(true, null);
-
-    this.setColour(180);
-
- this.setTooltip("");
-
- this.setHelpUrl("");
-
-  }
-
-};
-
-//Цикл пока x > <
-Blockly.Blocks['while_x'] = {
-  init: function() {
-    this.appendStatementInput("loop_body")
-        .setCheck(null)
-        .appendField("пока Х ")
-        .appendField(new Blockly.FieldDropdown([["больше",">"], ["меньше","<"]]), "comparing")
-        .appendField(new Blockly.FieldTextInput("0"), "value");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(180);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-//Цикл пока y > <
-Blockly.Blocks['while_y'] = {
-  init: function() {
-    this.appendStatementInput("loop_body")
-        .setCheck(null)
-        .appendField("пока Y ")
-        .appendField(new Blockly.FieldDropdown([["больше",">"], ["меньше","<"]]), "comparing")
-        .appendField(new Blockly.FieldTextInput("0"), "value");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(180);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-//Блок для сравнивания веса деталей
-Blockly.Blocks['if_part_weight'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Если");
-    this.appendValueInput("detail_1")
-        .setCheck(null);
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldDropdown([["больше",">"], ["меньше","<"]]), "condition");
-    this.appendValueInput("detail_2")
-        .setCheck(null);
-    this.appendDummyInput()
-        .appendField(", то");
-    this.appendStatementInput("condition_body")
-        .setCheck(null);
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(195);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-//Блок Деталь в рюкзаке
-Blockly.Blocks['in_a_backpack'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("вес детали");
-    this.appendDummyInput()
-        .appendField("в памяти");
-    this.setOutput(true, null);
-    this.setColour(210);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-//Блок Деталь под ногами
-Blockly.Blocks['on_the_floor'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("вес");
-    this.appendDummyInput()
-        .appendField("новой детали");
-    this.setOutput(true, null);
-    this.setColour(210);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-//Блок Заменить деталь
-Blockly.Blocks['replace_the_part'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Запомнить вес новой детали");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(220);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
-//цикл for
-Blockly.Blocks['for_loop'] = {
-  init: function() {
-    this.appendStatementInput("loop_body")
-        .setCheck(null)
-        .appendField("повторить")
-        .appendField(new Blockly.FieldTextInput("0"), "i")
-        .appendField("раз");
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(180);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
-
+//Тут создаются наши пользовательские блоки со всеми их параметрами
+
+//Для создания можно пользоваться этим приложением https://blockly-demo.appspot.com/static/demos/blockfactory/index.html
+
+
+
+Blockly.Blocks['go_right'] = {
+
+  init: function() {
+
+    this.appendDummyInput()
+
+        .appendField("Шаг вправо");
+
+    this.setPreviousStatement(true, null);
+
+    this.setNextStatement(true, null);
+
+    this.setColour(120);
+
+ this.setTooltip("Движение");
+
+ this.setHelpUrl("");
+
+  }
+
+};
+
+
+
+Blockly.Blocks['go_left'] = {
+
+  init: function() {
+
+    this.appendDummyInput()
+
+        .appendField("Шаг влево");
+
+    this.setPreviousStatement(true, null);
+
+    this.setNextStatement(true, null);
+
+    this.setColour(120);
+
+ this.setTooltip("Движение");
+
+ this.setHelpUrl("");
+
+  }
+
+};
+
+
+
+Blockly.Blocks['go_up'] = {
+
+  init: function() {
+
+    this.appendDummyInput()
+
+        .appendField("Шаг вверх");
+
+    this.setPreviousStatement(true, null);
+
+    this.setNextStatement(true, null);
+
+    this.setColour(120);
+
+ this.setTooltip("Движение");
+
+ this.setHelpUrl("");
+
+  }
+
+};
+
+
+
+Blockly.Blocks['go_down'] = {
+
+  init: function() {
+
+    this.appendDummyInput()
+
+        .appendField("Шаг вниз");
+
+    this.setPreviousStatement(true, null);
+
+    this.setNextStatement(true, null);
+
+    this.setColour(120);
+
+ this.setTooltip("Движение");
+
+ this.setHelpUrl("");
+
+  }
+
+};
+
+
+
+//цикл "Пока ... свободно"
+
+Blockly.Blocks['while_direction_free'] = {
+
+  init: function() {
+
+    this.appendStatementInput("loop_body")
+
+        .setCheck(null)
+
+        .setAlign(Blockly.ALIGN_CENTRE)
+
+        .appendField("Пока  ")
+
+        .appendField(new Blockly.FieldDropdown([["справа","right"], ["слева","left"], ["сверху","top"], ["снизу","down"]]), "direction")
+
+        .appendField("свободно");
+
+    this.setPreviousStatement(true, null);
+
+    this.setNextStatement(true, null);
+
+    this.setColour(150);
+
+ this.setTooltip("");
+
+ this.setHelpUrl("");
+
+  }
+
+};
+
+
+
+//цикл "Пока цель не достигнута"
+
+Blockly.Blocks['while_not_goal'] = {
+
+  init: function() {
+
+    this.appendStatementInput("loop_body")
+
+        .setCheck(null)
+
+        .setAlign(Blockly.ALIGN_CENTRE)
+
+        .appendField("Пока цель не достигнута")
+
+    this.setPreviousStatement(true, null);
+
+    this.setNextStatement(true, null);
+
+    this.setColour(150);
+
+ this.setTooltip("");
+
+ this.setHelpUrl("");
+
+  }
+
+};
+
+
+
+//условие "Если ... свободно"
+
+Blockly.Blocks['if_direction_free'] = {
+
+  init: function() {
+
+    this.appendStatementInput("condition_body")
+
+        .setCheck(null)
+
+        .appendField("Если ")
+
+        .appendField(new Blockly.FieldDropdown([["справа","right"], ["слева","left"], ["сверху","top"], ["снизу","down"]]), "direction")
+
+        .appendField("свободно");
+
+    this.setPreviousStatement(true, null);
+
+    this.setNextStatement(true, null);
+
+    this.setColour(180);
+
+ this.setTooltip("");
+
+ this.setHelpUrl("");
+
+  }
+
+};
+
+
+
+//условие "Если ... свободно, иначе"
+
+Blockly.Blocks['if_direction_free_else'] = {
+
+  init: function() {
+
+    this.appendStatementInput("condition_body")
+
+        .setCheck(null)
+
+        .appendField("Если ")
+
+        .appendField(new Blockly.FieldDropdown([["справа","right"], ["слева","left"], ["сверху","top"], ["снизу","down"]]), "direction")
+
+        .appendField("свободно");
+
+    this.appendStatementInput("else_body")
+
+        .setCheck(null)
+
+        .setAlign(Blockly.ALIGN_RIGHT)
+
+        .appendField("иначе");
+
+    this.setPreviousStatement(true, null);
+
+    this.setNextStatement(true, null);
+
+    this.setColour(180);
+
+ this.setTooltip("");
+
+ this.setHelpUrl("");
+
+  }
+
+};
+
+//Цикл пока x > <
+Blockly.Blocks['while_x'] = {
+  init: function() {
+    this.appendStatementInput("loop_body")
+        .setCheck(null)
+        .appendField("пока Х ")
+        .appendField(new Blockly.FieldDropdown([["больше",">"], ["меньше","<"]]), "comparing")
+        .appendField(new Blockly.FieldTextInput("0"), "value");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//Цикл пока y > <
+Blockly.Blocks['while_y'] = {
+  init: function() {
+    this.appendStatementInput("loop_body")
+        .setCheck(null)
+        .appendField("пока Y ")
+        .appendField(new Blockly.FieldDropdown([["больше",">"], ["меньше","<"]]), "comparing")
+        .appendField(new Blockly.FieldTextInput("0"), "value");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//Блок для сравнивания веса деталей
+Blockly.Blocks['if_part_weight'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Если");
+    this.appendValueInput("detail_1")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["больше",">"], ["меньше","<"]]), "condition");
+    this.appendValueInput("detail_2")
+        .setCheck(null);
+    this.appendDummyInput()
+        .appendField(", то");
+    this.appendStatementInput("condition_body")
+        .setCheck(null);
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(195);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//Блок Деталь в рюкзаке
+Blockly.Blocks['in_a_backpack'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("вес детали");
+    this.appendDummyInput()
+        .appendField("в памяти");
+    this.setOutput(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//Блок Деталь под ногами
+Blockly.Blocks['on_the_floor'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("вес");
+    this.appendDummyInput()
+        .appendField("новой детали");
+    this.setOutput(true, null);
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//Блок Заменить деталь
+Blockly.Blocks['replace_the_part'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Запомнить вес новой детали");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(220);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+//цикл for
+Blockly.Blocks['for_loop'] = {
+  init: function() {
+    this.appendStatementInput("loop_body")
+        .setCheck(null)
+        .appendField("повторить")
+        .appendField(new Blockly.FieldTextInput("0"), "i")
+        .appendField("раз");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(180);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
--- itgenio-blocks/js-generators.js
+++ itgenio-blocks/js-generators.js
@@ -1,212 +1,212 @@
-//Тут прописываем функции, возвращающие js код, генерируемый каждым нашим блоком
-
-
-
-Blockly.JavaScript['go_right'] = function(block) {
-
-  var code = 'if (!gameHerro.isGoal()) gameHerro.goRight();\n';
-
-  return code;
-
-};
-
-
-
-Blockly.JavaScript['go_left'] = function(block) {
-
-  var code = 'if (!gameHerro.isGoal()) gameHerro.goLeft();\n';
-
-  return code;
-
-};
-
-
-
-Blockly.JavaScript['go_up'] = function(block) {
-
-  var code = 'if (!gameHerro.isGoal()) gameHerro.goUp();\n';
-
-  return code;
-
-};
-
-
-
-Blockly.JavaScript['go_down'] = function(block) {
-
-  var code = 'if (!gameHerro.isGoal()) gameHerro.goDown();\n';
-
-  return code;
-
-};
-
-
-
-Blockly.JavaScript['while_direction_free'] = function(block) {
-
-  var dropdown_direction = block.getFieldValue('direction');
-
-  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
-
-  var code = `
-
-  while (gameHerro.isFree("${dropdown_direction}")) {
-    if (gameHerro.isGoal()) break;
-    ${statements_loop_body}
-
-  };\n`;
-
-  return code;
-
-};
-
-
-
-Blockly.JavaScript['while_not_goal'] = function(block) {
-
-  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
-
-  var code = `
- (function() {
-    var limitLoop = 1000;
-
-    while (!gameHerro.isGoal() && limitLoop > 0) {
-
-      limitLoop--;
-
-      ${statements_loop_body}
-
-    }
-  })();\n`;
-
-  return code;
-
-};
-
-
-
-Blockly.JavaScript['if_direction_free'] = function(block) {
-
-  var dropdown_direction = block.getFieldValue('direction');
-
-  var statements_condition_body = Blockly.JavaScript.statementToCode(block, 'condition_body');
-
-  var code = `
-
-  if (gameHerro.isFree("${dropdown_direction}") && !gameHerro.isGoal() ) {
-
-    ${statements_condition_body}
-
-  };\n`;
-
-  return code;
-
-};
-
-
-
-Blockly.JavaScript['if_direction_free_else'] = function(block) {
-
-  var dropdown_direction = block.getFieldValue('direction');
-
-  var statements_condition_body = Blockly.JavaScript.statementToCode(block, 'condition_body');
-
-  var statements_else_body = Blockly.JavaScript.statementToCode(block, 'else_body');
-
-  var code = `
-
-  if (gameHerro.isFree("${dropdown_direction}") && !gameHerro.isGoal()) {
-
-    ${statements_condition_body}
-
-  } else {
-
-    ${statements_else_body}
-
-  };\n`;
-
-  return code;
-
-};
-
-Blockly.JavaScript['while_x'] = function(block) {
-  var dropdown_comparing = block.getFieldValue('comparing');
-  var text_value = block.getFieldValue('value');
-  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
-  
-  var code = `
-  (function() {
-    var limitLoop = 1000;
-    while (gameHerro.x*50 ${dropdown_comparing} ${text_value} && !gameHerro.isGoal()) {
-      limitLoop--;
-    
-      if (gameHerro.isGoal()) break;
-      ${statements_loop_body}
-    }
-  })();\n`;
-
-  return code;
-};
-
-Blockly.JavaScript['while_y'] = function(block) {
-  var dropdown_comparing = block.getFieldValue('comparing');
-  var text_value = block.getFieldValue('value');
-  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
-  
-  var code = `
-
-  (function() {
-    var limitLoop = 1000;
-    while ((gameHerro.map.length - 1 - gameHerro.y) * 50 ${dropdown_comparing} ${text_value}
-    && !gameHerro.isGoal()) {
-    limitLoop--;
-    if (gameHerro.isGoal()) break;
-      ${statements_loop_body}
-    }
-  })();\n`;
-
-  return code;
-};
-
-Blockly.JavaScript['if_part_weight'] = function(block) {
-  var value_detail_1 = Blockly.JavaScript.valueToCode(block, 'detail_1', Blockly.JavaScript.ORDER_ATOMIC);
-  var dropdown_condition = block.getFieldValue('condition');
-  var value_detail_2 = Blockly.JavaScript.valueToCode(block, 'detail_2', Blockly.JavaScript.ORDER_ATOMIC);
-  var statements_condition_body = Blockly.JavaScript.statementToCode(block, 'condition_body');
-  var code = `
-  if (${value_detail_1} ${dropdown_condition} ${value_detail_2}) {
-    ${statements_condition_body}
-  }
-  ;\n`;
-  return code;
-};
-
-Blockly.JavaScript['in_a_backpack'] = function(block) {
-  var code = 'gameHerro.inBackpack';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['on_the_floor'] = function(block) {
-  var code = 'gameHerro.map[gameHerro.y][gameHerro.x]';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['replace_the_part'] = function(block) {
-  var code = `
-  var d = gameHerro.inBackpack;
-  gameHerro.inBackpack = gameHerro.map[gameHerro.y][gameHerro.x];
-  gameHerro.map[gameHerro.y][gameHerro.x] = d
-  ;\n`;
-  return code;
-};
-
-Blockly.JavaScript['for_loop'] = function(block) {
-  var text_i = block.getFieldValue('i');
-  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
-  var code = `
-  for (var i = 0; i < ${text_i}; i++) {
-    ${statements_loop_body}
-  }
-  ;\n`;
-  return code;
+//Тут прописываем функции, возвращающие js код, генерируемый каждым нашим блоком
+
+
+
+Blockly.JavaScript['go_right'] = function(block) {
+
+  var code = 'if (!gameHerro.isGoal()) gameHerro.goRight();\n';
+
+  return code;
+
+};
+
+
+
+Blockly.JavaScript['go_left'] = function(block) {
+
+  var code = 'if (!gameHerro.isGoal()) gameHerro.goLeft();\n';
+
+  return code;
+
+};
+
+
+
+Blockly.JavaScript['go_up'] = function(block) {
+
+  var code = 'if (!gameHerro.isGoal()) gameHerro.goUp();\n';
+
+  return code;
+
+};
+
+
+
+Blockly.JavaScript['go_down'] = function(block) {
+
+  var code = 'if (!gameHerro.isGoal()) gameHerro.goDown();\n';
+
+  return code;
+
+};
+
+
+
+Blockly.JavaScript['while_direction_free'] = function(block) {
+
+  var dropdown_direction = block.getFieldValue('direction');
+
+  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
+
+  var code = `
+
+  while (gameHerro.isFree("${dropdown_direction}")) {
+    if (gameHerro.isGoal()) break;
+    ${statements_loop_body}
+
+  };\n`;
+
+  return code;
+
+};
+
+
+
+Blockly.JavaScript['while_not_goal'] = function(block) {
+
+  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
+
+  var code = `
+ (function() {
+    var limitLoop = 1000;
+
+    while (!gameHerro.isGoal() && limitLoop > 0) {
+
+      limitLoop--;
+
+      ${statements_loop_body}
+
+    }
+  })();\n`;
+
+  return code;
+
+};
+
+
+
+Blockly.JavaScript['if_direction_free'] = function(block) {
+
+  var dropdown_direction = block.getFieldValue('direction');
+
+  var statements_condition_body = Blockly.JavaScript.statementToCode(block, 'condition_body');
+
+  var code = `
+
+  if (gameHerro.isFree("${dropdown_direction}") && !gameHerro.isGoal() ) {
+
+    ${statements_condition_body}
+
+  };\n`;
+
+  return code;
+
+};
+
+
+
+Blockly.JavaScript['if_direction_free_else'] = function(block) {
+
+  var dropdown_direction = block.getFieldValue('direction');
+
+  var statements_condition_body = Blockly.JavaScript.statementToCode(block, 'condition_body');
+
+  var statements_else_body = Blockly.JavaScript.statementToCode(block, 'else_body');
+
+  var code = `
+
+  if (gameHerro.isFree("${dropdown_direction}") && !gameHerro.isGoal()) {
+
+    ${statements_condition_body}
+
+  } else {
+
+    ${statements_else_body}
+
+  };\n`;
+
+  return code;
+
+};
+
+Blockly.JavaScript['while_x'] = function(block) {
+  var dropdown_comparing = block.getFieldValue('comparing');
+  var text_value = block.getFieldValue('value');
+  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
+  
+  var code = `
+  (function() {
+    var limitLoop = 1000;
+    while (gameHerro.x*50 ${dropdown_comparing} ${text_value} && !gameHerro.isGoal()) {
+      limitLoop--;
+    
+      if (gameHerro.isGoal()) break;
+      ${statements_loop_body}
+    }
+  })();\n`;
+
+  return code;
+};
+
+Blockly.JavaScript['while_y'] = function(block) {
+  var dropdown_comparing = block.getFieldValue('comparing');
+  var text_value = block.getFieldValue('value');
+  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
+  
+  var code = `
+
+  (function() {
+    var limitLoop = 1000;
+    while ((gameHerro.map.length - 1 - gameHerro.y) * 50 ${dropdown_comparing} ${text_value}
+    && !gameHerro.isGoal()) {
+    limitLoop--;
+    if (gameHerro.isGoal()) break;
+      ${statements_loop_body}
+    }
+  })();\n`;
+
+  return code;
+};
+
+Blockly.JavaScript['if_part_weight'] = function(block) {
+  var value_detail_1 = Blockly.JavaScript.valueToCode(block, 'detail_1', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_condition = block.getFieldValue('condition');
+  var value_detail_2 = Blockly.JavaScript.valueToCode(block, 'detail_2', Blockly.JavaScript.ORDER_ATOMIC);
+  var statements_condition_body = Blockly.JavaScript.statementToCode(block, 'condition_body');
+  var code = `
+  if (${value_detail_1} ${dropdown_condition} ${value_detail_2}) {
+    ${statements_condition_body}
+  }
+  ;\n`;
+  return code;
+};
+
+Blockly.JavaScript['in_a_backpack'] = function(block) {
+  var code = 'gameHerro.inBackpack';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['on_the_floor'] = function(block) {
+  var code = 'gameHerro.map[gameHerro.y][gameHerro.x]';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['replace_the_part'] = function(block) {
+  var code = `
+  var d = gameHerro.inBackpack;
+  gameHerro.inBackpack = gameHerro.map[gameHerro.y][gameHerro.x];
+  gameHerro.map[gameHerro.y][gameHerro.x] = d
+  ;\n`;
+  return code;
+};
+
+Blockly.JavaScript['for_loop'] = function(block) {
+  var text_i = block.getFieldValue('i');
+  var statements_loop_body = Blockly.JavaScript.statementToCode(block, 'loop_body');
+  var code = `
+  for (var i = 0; i < ${text_i}; i++) {
+    ${statements_loop_body}
+  }
+  ;\n`;
+  return code;
 };
